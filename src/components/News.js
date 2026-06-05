af12f18494f07fb83cc882a369724c5a79f3c3e5
RAJ KUMAR <rajkumar.contact04@gmail.com>
2026-06-06 04:51:06 +0530

    Use live news only

--- src/components/News.js
+++ src/components/News.js
@@ -2,45 +2,59 @@ import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 
 export class News extends Component {
+  constructor(props) {
+    super(props);
 
-  componentDidMount() {
-  let url = "https://newsapi.org/v2/everything?q=india&apiKey=c051cc2707dc4b6485b99584bfdcdb37";
-
-fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log("API RESPONSE:", data);
-console.log("ARTICLES:", data.articles);
-    if (data.articles) {
-      this.setState({
-        articles: data.articles
-      });
-    }
-  })
-  .catch((error) => {
-    console.error("Error fetching news:", error);
-  });
-}
-
-  render() {
-    console.log("STATE ARTICLES:", this.state.articles);
-    let filteredArticles = (this.state.articles || []).filter((article) => {
-  if (!this.props.searchText) {
-    return true;
+    this.state = {
+      articles: [
+        {
+          title: "Live news could not load",
+          description: "Showing fallback news. Please try again later.",
+          url: "https://news.google.com",
+          urlToImage: "/no-image.png"
+        }
+      ]
+    };
   }
 
-  let search = this.props.searchText.toLowerCase();
+  componentDidMount() {
+    let url = "https://newsapi.org/v2/everything?q=india&apiKey=c051cc2707dc4b6485b99584bfdcdb37";
 
-  let title = article.title || "";
-  let description = article.description || "";
-  let content = article.content || "";
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log("API RESPONSE:", data);
+        console.log("ARTICLES:", data.articles);
 
-  return (
-    title.toLowerCase().includes(search) ||
-    description.toLowerCase().includes(search) ||
-    content.toLowerCase().includes(search)
+        if (data.articles && data.articles.length > 0) {
+          this.setState({
+            articles: data.articles
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching news:", error);
+      });
+  }
+
+  render() {
+    let filteredArticles = (this.state.articles || []).filter((article) => {
+      if (!this.props.searchText) {
+        return true;
+      }
+
+      let search = this.props.searchText.toLowerCase();
+
+      let title = article.title || "";
+      let description = article.description || "";
+      let content = article.content || "";
+
+      return (
+        title.toLowerCase().includes(search) ||
+        description.toLowerCase().includes(search) ||
+        content.toLowerCase().includes(search)
       );
     });
 
@@ -51,7 +65,7 @@ console.log("ARTICLES:", data.articles);
         <div className="row">
           {filteredArticles.map((element) => {
             return (
-              <div className="col-md-4" key={element.title}>
+              <div className="col-md-4" key={element.url || element.title}>
                 <Newsitem
                   title={element.title}
                   description={element.description}
